6d02ea7fe6610b99edeaa391e729846c6bb6acea
AnushaGampa <anushagampa1208@gmail.com>
2023-04-17 23:45:50 +0100

    JIR-13 API 3:Get ALl Brands List testcases

---
+++ src/test/java/api/endpoints/anu/BrandsItem.java
@@ -0,0 +1,20 @@
+package api.endpoints.anu;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class BrandsItem{
+
+	@JsonProperty("id")
+	private Integer id;
+
+	@JsonProperty("brand")
+	private String brand;
+
+	public Integer getId(){
+		return id;
+	}
+
+	public String getBrand(){
+		return brand;
+	}
+}
