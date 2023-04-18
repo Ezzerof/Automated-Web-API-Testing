207ff310b3c42249290e5cfb89b6fd683f74a343
bakar212 <a.khan480849@gmail.com>
2023-04-18 10:40:45 +0100

    Moved Test Cases from AppTest.java to Seperate test class called LoginAndEmailTest.java

--- src/test/java/api/AppTest.java
+++ src/test/java/api/AppTest.java
@@ -15,32 +15,32 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class AppTest {
 
-    @Order(8)
-    @Test
-    @DisplayName("Testing API POST request for logging in without e-mail parameter")
-    void verifyForbiddenLoginWithoutEmailParameter(){
-
-        Response response = given().contentType("application/x-www-form-urlencoded").formParams("password","password")
-                .post(Routes.postLoginDetails_url);
-
-        response.jsonPath().get("message").equals("This request method is not supported.");
-
-    }
-
-    @Order(14)
-    @Test
-    @DisplayName("Testing API GET request for accessing user details by email")
-    void GETUserAccountByEmail(){
-
-        //when().get(Routes.getUserAccountByEmail_url).then().body("message",
-        //        Matchers.equalTo("Bad request, email parameter is missing in GET request."));
-
-        when().get(Routes.getUserAccountByEmail_url).getBody().print();
-        assertThat(when().get(Routes.getUserAccountByEmail_url).jsonPath().get("message")
-                ,Matchers.equalTo("Bad request, email parameter is missing in GET request."));
-
-
-    }
+//    @Order(8)
+//    @Test
+//    @DisplayName("Testing API POST request for logging in without e-mail parameter")
+//    void verifyForbiddenLoginWithoutEmailParameter(){
+//
+//        Response response = given().contentType("application/x-www-form-urlencoded").formParams("password","password")
+//                .post(Routes.postLoginDetails_url);
+//
+//        response.jsonPath().get("message").equals("This request method is not supported.");
+//
+//    }
+//
+//    @Order(14)
+//    @Test
+//    @DisplayName("Testing API GET request for accessing user details by email")
+//    void GETUserAccountByEmail(){
+//
+//        //when().get(Routes.getUserAccountByEmail_url).then().body("message",
+//        //        Matchers.equalTo("Bad request, email parameter is missing in GET request."));
+//
+//        when().get(Routes.getUserAccountByEmail_url).getBody().print();
+//        assertThat(when().get(Routes.getUserAccountByEmail_url).jsonPath().get("message")
+//                ,Matchers.equalTo("Bad request, email parameter is missing in GET request."));
+//
+//
+//    }
 
 
 
---
+++ src/test/java/api/LoginAndEmailTest.java
@@ -0,0 +1,45 @@
+package api;
+
+import api.endpoints.Routes;
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class LoginAndEmailTest {
+
+    @Order(8)
+    @Test
+    @DisplayName("Testing API POST request for logging in without e-mail parameter")
+    void verifyForbiddenLoginWithoutEmailParameter(){
+
+        Response response = given().contentType("application/x-www-form-urlencoded").formParams("password","password")
+                .post(Routes.postLoginDetails_url);
+
+        response.jsonPath().get("message").equals("This request method is not supported.");
+
+    }
+
+    @Order(14)
+    @Test
+    @DisplayName("Testing API GET request for accessing user details by email")
+    void GETUserAccountByEmail(){
+
+        //when().get(Routes.getUserAccountByEmail_url).then().body("message",
+        //        Matchers.equalTo("Bad request, email parameter is missing in GET request."));
+
+        when().get(Routes.getUserAccountByEmail_url).getBody().print();
+        assertThat(when().get(Routes.getUserAccountByEmail_url).jsonPath().get("message")
+                , Matchers.equalTo("Bad request, email parameter is missing in GET request."));
+
+
+    }
+
+
+
+}
