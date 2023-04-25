bbfa5cc9f9bda2446b2d31fdc75ff8c6cb1b5af4
bakar212 <a.khan480849@gmail.com>
2023-04-25 11:59:17 +0100

    Completion of methods on HomePage.java class to aid with subscription step defs

--- src/test/java/web/cucumber/pages/HomePage.java
+++ src/test/java/web/cucumber/pages/HomePage.java
@@ -73,15 +73,17 @@ public class HomePage {
     }
 
     public String getContentOfFooterElementH2(){
-        return  null;
+
+        return webDriver.findElement(By.cssSelector(".single-widget h2")).getText();
     }
 
     public void enterEmailInFooter(String Email){
 
+        webDriver.findElement(By.cssSelector(".searchform input")).sendKeys(Email);
     }
 
-    public void submitEmailArrow(){
-
+    public void clickArrowSubmitButton(){
+        webDriver.findElement(By.cssSelector(".searchform button i")).click();
     }
 
 
